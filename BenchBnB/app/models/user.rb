3d68991f4ec621316b01336e4d9daf94e2420995
hwkcode <hwkcode@gmail.com>
2022-02-22 14:53:20 -0500

    Create sessions controller and complete user model

--- BenchBnB/app/models/user.rb
+++ BenchBnB/app/models/user.rb
@@ -1,3 +1,33 @@
 class User < ApplicationRecord
+    after_initialize :ensure_session_token
     
+    validates :username, :password_digest, :session_token, presence: true
+
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+        if @user && @user.is_password?(password)
+            @user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
 end
